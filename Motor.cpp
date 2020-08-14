c358532508b51f3a52b57e05c9600543289efd86
wems99 <allen.blanco.contreras@est.una.ac.cr>
2020-08-14 11:24:52 -0600

    class motor.h and motor.cpp

---
+++ Motor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Usuario on 14/8/2020.
+//
+
+#include "Motor.h"
---
+++ Motor.h
@@ -0,0 +1,18 @@
+//
+// Created by Usuario on 14/8/2020.
+//
+
+#ifndef BASIC_00_EXAMPLE_MOTOR_H
+#define BASIC_00_EXAMPLE_MOTOR_H
+
+
+class Motor {
+
+private:
+
+    int
+
+};
+
+
+#endif //BASIC_00_EXAMPLE_MOTOR_H
