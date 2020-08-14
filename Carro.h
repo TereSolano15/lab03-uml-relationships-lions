9c761b68e9208ceb67658c0b86ec925e623b4ee8
fabio0610 <fabio0610@hotmail.com>
2020-08-14 11:49:29 -0600

    arreglos

---
+++ Carro.h
@@ -0,0 +1,24 @@
+//
+// Created by Fabio Villalobos on 14/8/2020.
+//
+
+#ifndef LAB03_UML_RELATIONSHIP_LIONS_CARRO_H
+#define LAB03_UML_RELATIONSHIP_LIONS_CARRO_H
+
+#endif //LAB03_UML_RELATIONSHIP_LIONS_CARRO_H
+
+#pragma once
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+class Carro{
+private:
+    int cilindrada;
+    int modelo;
+    int numeroPasajeros;
+    string tipoCombustible;
+public:
+
+};
