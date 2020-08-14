1a6795eafcc3f877e41b66bc35dbabe14b2795db
TereSolano15 <63326038+TereSolano15@users.noreply.github.com>
2020-08-14 11:06:16 -0600

    Chofer

--- Chofer.cpp
+++ Chofer.cpp
@@ -5,3 +5,35 @@
 #include "Chofer.h"
 
 Chofer::Chofer(const string &name, const string &id) : name(name), id(id) {}
+
+Chofer::Chofer(const string &name, const string &id, Auto *audi) : name(name), id(id), audi(audi) {}
+
+Chofer::~Chofer() {
+
+}
+
+const string &Chofer::getName() const {
+    return name;
+}
+
+void Chofer::setName(const string &name) {
+    Chofer::name = name;
+}
+
+const string &Chofer::getId() const {
+    return id;
+}
+
+void Chofer::setId(const string &id) {
+    Chofer::id = id;
+}
+
+
+
+Auto *Chofer::getAudi() const {
+    return audi;
+}
+
+void Chofer::setAudi(Auto *audi) {
+    Chofer::audi = audi;
+}
