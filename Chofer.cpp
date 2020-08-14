432211a6667e71831371e21340f8e46ae0d79f86
TereSolano15 <63326038+TereSolano15@users.noreply.github.com>
2020-08-14 10:56:19 -0600

    Cmake

---
+++ Chofer.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Tere Solano on 14/8/2020.
+//
+
+#include "Chofer.h"
+
+Chofer::Chofer(const string &name, const string &id) : name(name), id(id) {}
---
+++ Chofer.h
@@ -0,0 +1,19 @@
+//
+// Created by Tere Solano on 14/8/2020.
+//
+
+#ifndef LAB03_UML_RELATIONSHIPS_LIONS_CHOFER_H
+#define LAB03_UML_RELATIONSHIPS_LIONS_CHOFER_H
+#include <iostream>
+
+class Chofer {
+private:
+    string name;
+    string id;
+public:
+    Chofer(const string &name, const string &id);
+    
+};
+
+
+#endif //LAB03_UML_RELATIONSHIPS_LIONS_CHOFER_H
